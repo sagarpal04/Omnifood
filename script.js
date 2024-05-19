5fb32f869c06cbceee32edfcb2c37f677ec567f5
sagarpal04 <sagarpal0402@gmail.com>
2024-05-19 10:15:57 +0530

    changes

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
